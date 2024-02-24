dc8838c79a5a7c994da78496b67119a5037b8fbb
shoonia <alexanderz@wix.com>
2024-02-24 21:16:26 +0200

    Init package

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+import pkg from './package.json' with { type: 'json' };
+
+const extensions = ['.ts'];
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.exports,
+      exports: 'default',
+      format: 'cjs',
+    },
+  ],
+  external: Object.keys(pkg.dependencies),
+  plugins: [
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+      comments: false,
+      presets: [
+        '@babel/preset-typescript',
+      ],
+      comments: false,
+    }),
+    nodeResolve({ extensions }),
+  ],
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { declare as declarePlugin } from '@babel/helper-plugin-utils';
+
+const plugin = declarePlugin((api) => {});
+
+export { plugin as default };
