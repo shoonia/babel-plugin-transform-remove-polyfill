3a282c5065112efac8dffab1947b37e2b9e6a0cb
shoonia <alexanderz@wix.com>
2024-04-03 19:20:17 +0300

    Add prototype

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,35 @@
 import t from '@babel/types';
 
+type SP = keyof typeof String.prototype;
+type AP = keyof Array<null>;
 type A = keyof ArrayConstructor
 type O = keyof ObjectConstructor;
 type R = keyof typeof Reflect;
+
+const arrayProtoKeys = new Set<string>([
+  'keys', //      38
+  'entries', //   38
+  'find', //      45
+  'findIndex', // 45
+  'fill', //      45
+  'includes', //  47
+  'flat', //      69
+  'flatMap', //   69
+] satisfies AP[]);
+
+const stringProtoKeys = new Set<string>([
+  'trim', //        4
+  'normalize', //   34
+  'includes', //    41
+  'startsWith', //  41
+  'endsWith', //    41
+  'repeat', //      41
+  'codePointAt', // 41
+  'padStart', //    57
+  'padEnd', //      57
+  'trimStart', //   66
+  'trimEnd', //     66
+] satisfies SP[]);
 
 const objectKeys = new Set<string>([
   'assign',
@@ -86,6 +113,18 @@ export const repliceGroup = (node: t.Node): node is t.MemberExpression => {
           return oneOfIdentifier(node.property, arrayKeys);
         case 'Reflect':
           return oneOfIdentifier(node.property, reflectKeys);
+      }
+    }
+    else if (
+      t.isMemberExpression(node.object, { computed: false }) &&
+      t.isIdentifier(node.object.property, { name: 'prototype' }) &&
+      t.isIdentifier(node.object.object, null)
+    ) {
+      switch (node.object.object.name) {
+        case 'Array':
+          return oneOfIdentifier(node.property, arrayProtoKeys);
+        case 'String':
+          return oneOfIdentifier(node.property, stringProtoKeys);
       }
     }
   }
---
+++ test/String.prototype.includes.spec.ts
@@ -0,0 +1,93 @@
+import { describe, test } from 'node:test';
+import { expect } from './setup';
+
+String.prototype.includes;
+
+describe('String.prototype.includes', () => {
+  const trueList = [
+    'typeof String.prototype.includes === "function"',
+    'typeof String.prototype.includes == "function"',
+    '"function" === typeof String.prototype.includes',
+    '"function" == typeof String.prototype.includes',
+    // undefined
+    'typeof String.prototype.includes !== "undefined"',
+    'typeof String.prototype.includes != "undefined"',
+    '"undefined" !== typeof String.prototype.includes',
+    '"undefined" != typeof String.prototype.includes',
+  ];
+
+  trueList.forEach((code) => {
+    test(code, async () => {
+      await expect(code).toBeTransform('true;');
+    });
+  });
+
+  const falseList =  [
+    'typeof String.prototype.includes !== "function"',
+    'typeof String.prototype.includes != "function"',
+    '"function" !== typeof String.prototype.includes',
+    '"function" != typeof String.prototype.includes',
+    // undefined
+    'typeof String.prototype.includes === "undefined"',
+    'typeof String.prototype.includes == "undefined"',
+    '"undefined" === typeof String.prototype.includes',
+    '"undefined" == typeof String.prototype.includes',
+  ];
+
+  falseList.forEach((code) => {
+    test(code, async () => {
+      await expect(code).toBeTransform('false;');
+    });
+  });
+
+  const ternaryList = [
+    'typeof String.prototype.includes === "function" ? 1 : 2',
+    'typeof String.prototype.includes !== "function" ? 2 : 1',
+    'typeof String.prototype.includes !== "undefined" ? 1 : 2',
+    'typeof String.prototype.includes === "undefined" ? 2 : 1',
+    // ==
+    'typeof String.prototype.includes == "function" ? 1 : 2',
+    'typeof String.prototype.includes != "function" ? 2 : 1',
+    'typeof String.prototype.includes != "undefined" ? 1 : 2',
+    'typeof String.prototype.includes == "undefined" ? 2 : 1',
+    // ..
+    '"function" === typeof String.prototype.includes ? 1 : 2',
+    '"function" !== typeof String.prototype.includes ? 2 : 1',
+    '"undefined" !== typeof String.prototype.includes ? 1 : 2',
+    '"undefined" === typeof String.prototype.includes ? 2 : 1',
+  ];
+
+  ternaryList.forEach((code) => {
+    test(code, async () => {
+      await expect(code).toBeTransform('1;');
+    });
+  });
+
+  test('tranfrom #0', async () => {
+    await expect('if (String.prototype.includes) {}').toBeTransform('if (true) {}');
+  });
+
+  test('tranfrom #1', async () => {
+    await expect('String.prototype.includes || 1').toBeTransform('String.prototype.includes;');
+  });
+
+  test('tranfrom #2', async () => {
+    await expect('String.prototype.includes && 1').toBeTransform('1;');
+  });
+
+  test('tranfrom #3', async () => {
+    await expect('String.prototype.includes ? 1 : 2').toBeTransform('1;');
+  });
+
+  test('tranfrom #4', async () => {
+    await expect('typeof String.prototype.includes === "function" ? 1 : 2').toBeTransform('1;');
+  });
+
+  test('tranfrom #5', async () => {
+    await expect('typeof String.prototype.includes !== "undefined" ? 1 : 2').toBeTransform('1;');
+  });
+
+  test('tranfrom #6', async () => {
+    await expect('"undefined" == typeof String.prototype.includes ? 1 : 2').toBeTransform('2;');
+  });
+});
