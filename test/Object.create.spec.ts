4fc5f7cc71be6f801b1c9a6ec13db08874f597ab
shoonia <alexanderz@wix.com>
2024-04-08 00:16:03 +0300

    Add tests

--- test/Object.create.spec.ts
+++ test/Object.create.spec.ts
@@ -15,4 +15,30 @@ describe('Object.create', () => {
       return new b
   }`).toBeTransform('var qa = Object.create;');
   });
+
+  test('transform #2', async () => {
+    await expect(`var g = Object.create ? function(e, t, n, r) {
+      void 0 === r && (r = n);
+      var o = Object.getOwnPropertyDescriptor(t, n);
+      o && !("get"in o ? !t.__esModule : o.writable || o.configurable) || (o = {
+          enumerable: !0,
+          get: function() {
+              return t[n]
+          }
+      }),
+      Object.defineProperty(e, r, o)
+  } : function(e, t, n, r) {
+      void 0 === r && (r = n),
+      e[r] = t[n]
+  };`).toBeTransform(`var g = function (e, t, n, r) {
+  void 0 === r && (r = n);
+  var o = Object.getOwnPropertyDescriptor(t, n);
+  o && !("get" in o ? !t.__esModule : o.writable || o.configurable) || (o = {
+    enumerable: !0,
+    get: function () {
+      return t[n];
+    }
+  }), Object.defineProperty(e, r, o);
+};`);
+  });
 });
---
+++ test/Reflect.construct.spec.ts
@@ -0,0 +1,39 @@
+import { describe, test } from 'node:test';
+import { expect } from './setup';
+
+Reflect.construct;
+
+describe('Reflect.construct', () => {
+  test('typeof', () => {
+    expect(typeof Reflect.construct).toBe('function');
+  });
+
+  test('transform #1', async () => {
+    expect('undefined' == typeof Reflect || !Reflect.construct).toBe(false);
+
+    await expect(`function A() {
+    if ("undefined" == typeof Reflect || !Reflect.construct)
+        return !1;
+    if (Reflect.construct.sham)
+        return !1;
+    if ("function" == typeof Proxy)
+        return !0;
+    try {
+        return Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], (function() {}
+        ))),
+        !0
+    } catch (e) {
+        return !1
+    }
+  }`).toBeTransform(`function A() {
+  if (!Reflect.construct) return !1;
+  if (Reflect.construct.sham) return !1;
+  if (true) return !0;
+  try {
+    return Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})), !0;
+  } catch (e) {
+    return !1;
+  }
+}`);
+  });
+});
