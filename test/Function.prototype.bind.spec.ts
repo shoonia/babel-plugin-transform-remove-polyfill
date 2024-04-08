f6692e1e5c3e87c0607e50b63d8972cad0c11acd
shoonia <alexanderz@wix.com>
2024-04-08 20:04:13 +0300

    Add test

--- test/Function.prototype.bind.spec.ts
+++ test/Function.prototype.bind.spec.ts
@@ -8,7 +8,7 @@ describe('Function.prototype.bind', () => {
     expect(typeof Function.prototype.bind).toBe('function');
   });
 
-  test('transform #1', async () => {
+  test('transform #0', async () => {
     await expect(`var Ea = function(a, b, c) {
       Ea = Function.prototype.bind && -1 != Function.prototype.bind.toString().indexOf("native code") ? Ba : Ca;
       return Ea.apply(null, arguments)
@@ -17,5 +17,26 @@ describe('Function.prototype.bind', () => {
   Ea = -1 != Function.prototype.bind.toString().indexOf("native code") ? Ba : Ca;
   return Ea.apply(null, arguments);
 };`);
+  });
+
+  test('transform #1', async () => {
+    await expect(`Function.prototype.bind || (Function.prototype.bind = function(t) {
+  var n = this;
+  if (typeof n != "function")
+      throw new TypeError("Function.prototype.bind called on incompatible " + n);
+  var i = u.call(arguments, 1)
+    , s = function() {
+      if (this instanceof s) {
+          var e = n.apply(this, i.concat(u.call(arguments)));
+          return Object(e) === e ? e : this
+      }
+      return n.apply(t, i.concat(u.call(arguments)))
+  };
+  return n.prototype && (r.prototype = n.prototype,
+  s.prototype = new r,
+  r.prototype = null),
+  s
+});`
+    ).toBeTransform('Function.prototype.bind;');
   });
 });
