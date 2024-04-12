c3f51c8a59ba93bbe14788b4151ba0983fc7e5b6
shoonia <alexanderz@wix.com>
2024-04-12 18:37:29 +0300

    Add test

--- test/Object.getOwnPropertySymbols.spec.ts
+++ test/Object.getOwnPropertySymbols.spec.ts
@@ -8,7 +8,7 @@ describe('Object.getOwnPropertySymbols', () => {
     expect(typeof Object.getOwnPropertySymbols).toBe('function');
   });
 
-  test('transform #1', async () => {
+  test('transform #0', async () => {
     await expect(`function a(t, d) {
       var e, n = Object.keys(t);
       return Object.getOwnPropertySymbols && (e = Object.getOwnPropertySymbols(t), d && (e = e.filter(function(d) {
@@ -22,5 +22,26 @@ describe('Object.getOwnPropertySymbols', () => {
     return Object.getOwnPropertyDescriptor(t, d).enumerable;
   })), n.push.apply(n, e)), n;
 }`);
+  });
+
+  test('transform #1', async () => {
+    await expect(`var o = this && this.__rest || function(e, t) {
+  var n = {};
+  for (var r in e) Object.prototype.hasOwnProperty.call(e, r) && t.indexOf(r) < 0 && (n[r] = e[r]);
+  if (null != e && "function" == typeof Object.getOwnPropertySymbols) {
+    var o = 0;
+    for (r = Object.getOwnPropertySymbols(e); o < r.length; o++) t.indexOf(r[o]) < 0 && Object.prototype.propertyIsEnumerable.call(e, r[o]) && (n[r[o]] = e[r[o]]);
+  }
+  return n;
+};`)
+      .toBeTransform(`var o = this && this.__rest || function (e, t) {
+  var n = {};
+  for (var r in e) Object.hasOwn(e, r) && t.indexOf(r) < 0 && (n[r] = e[r]);
+  if (null != e && true) {
+    var o = 0;
+    for (r = Object.getOwnPropertySymbols(e); o < r.length; o++) t.indexOf(r[o]) < 0 && Object.prototype.propertyIsEnumerable.call(e, r[o]) && (n[r[o]] = e[r[o]]);
+  }
+  return n;
+};`);
   });
 });
