cd7ee180474acd5f64ec071dff4dfadfb0b46998
shoonia <alexanderz@wix.com>
2024-04-25 21:59:32 +0300

    Add test

--- test/Object.create.spec.ts
+++ test/Object.create.spec.ts
@@ -68,4 +68,31 @@ describe('Object.create', () => {
       }
   }`).toBeTransform('');
   });
+
+  test('transform #3', async () => {
+    await expect(`var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+      if (k2 === undefined) k2 = k;
+      var desc = Object.getOwnPropertyDescriptor(m, k);
+      if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+        desc = { enumerable: true, get: function() { return m[k]; } };
+      }
+      Object.defineProperty(o, k2, desc);
+    }) : (function(o, m, k, k2) {
+      if (k2 === undefined) k2 = k;
+      o[k2] = m[k];
+    }));`
+    ).toBeTransform(`var __createBinding = this && this.__createBinding || function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  var desc = Object.getOwnPropertyDescriptor(m, k);
+  if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+    desc = {
+      enumerable: true,
+      get: function () {
+        return m[k];
+      }
+    };
+  }
+  Object.defineProperty(o, k2, desc);
+};`);
+  });
 });
