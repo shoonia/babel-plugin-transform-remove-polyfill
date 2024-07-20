46df7b1f9fe86b6d9e013e065673f91b59833177
shoonia <alexanderz@wix.com>
2024-07-20 09:23:16 +0300

    Update ESLint

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+  {
+    ignores: [
+      'dist',
+    ],
+  },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/semi': 'error',
+      '@typescript-eslint/no-extra-semi': 'error',
+      'require-await': 'error',
+      'comma-dangle': [
+        'error',
+        'always-multiline',
+      ],
+      'indent': [
+        'error',
+        2,
+        {
+          'SwitchCase': 1,
+        },
+      ],
+      'quotes': [
+        'error',
+        'single',
+      ],
+      'space-before-function-paren': [
+        'error',
+        {
+          'anonymous': 'always',
+          'named': 'never',
+          'asyncArrow': 'always',
+        },
+      ],
+      'no-else-return': 'error',
+      'no-trailing-spaces': 'error',
+      'no-use-before-define': 'error',
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          'max': 1,
+          'maxBOF': 0,
+          'maxEOF': 0,
+        },
+      ],
+    },
+  },
+);
