ac5096eafab47b73776b8227fbd70b1ac37aaa52
shoonia <alexanderz@wix.com>
2025-08-19 11:23:44 +0300

    test: add additional transformation test for Promise.prototype.finally

--- test/Promise.prototype.finally.spec.ts
+++ test/Promise.prototype.finally.spec.ts
@@ -11,4 +11,23 @@ describe('Promise.prototype.finally', () => {
   test('transform #0', async () => {
     await expect('if (Promise.prototype.finally) {}').toBeTransform('if (true) {}');
   });
+
+  test('transform #1', async () => {
+    await expect(`Promise.prototype.finally || (Promise.prototype.finally = function(e) {
+    if ("function" != typeof e)
+        return this.then(e, e);
+    var t = this.constructor || Promise;
+    return this.then((function(n) {
+        return t.resolve(e()).then((function() {
+            return n
+        }
+        ))
+    }), (function(n) {
+        return t.resolve(e()).then((function() {
+            throw n
+        }))
+    }))
+})`)
+      .toBeTransform('');
+  });
 });
